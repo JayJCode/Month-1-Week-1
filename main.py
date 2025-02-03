ec6e06d389efda6205d5596310e4ff06c18c129d
Jan Janowicz <JayJCode@gmail.com>
2025-02-03 14:01:08 +0100

    argparse added

--- main.py
+++ main.py
@@ -14,24 +14,31 @@ import os
 from datetime import datetime
 import argparse
 
+# 4th part.
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="The name of the file we filter through")
+parser.add_argument("-k", "--key", help="The key name or symbol to be filtered in file")
+args = parser.parse_args()
+
 # 5th part. Using sys to get file_name, and os to get some information about this file.
 # Lastly using datetime for changing format to a readable for human being
-file_name = sys.argv[1]
-if not os.path.exists(file_name):
+
+# file_name = sys.argv[1] // no longer in use, since we use argparse
+if not os.path.exists(args.file):
     print("There is not a such file")
     exit(1)
-file_size = os.stat(file_name).st_size
-file_mod_time = datetime.fromtimestamp(os.stat(file_name).st_mtime)
+file_size = os.stat(args.file).st_size
+file_mod_time = datetime.fromtimestamp(os.stat(args.file).st_mtime)
 print(("ABOUT CHOSEN FILE:\n" +
        "File name: {}\n" +
        "File size: {}\n" +
-       "Last modified: {}\n").format(file_name, file_size, file_mod_time))
+       "Last modified: {}\n").format(args.file, file_size, file_mod_time))
 
-with open(file_name) as file:
+with open(args.file) as file:
     data = file.read() # 1st part.
     num_of_words = len(data.replace("\n", " ").strip().split(" ")) # 2nd part.
     num_of_lines = len(data.split("\n"))
-    index = data.index("key") # 3rd part.
+    index = data.index(args.key) # 3rd part. // use of args added
     print(("ABOUT ITS CONTENT:\n" +
            "Number of words: {}\n" +
            "Number of lines: {}\n" +
