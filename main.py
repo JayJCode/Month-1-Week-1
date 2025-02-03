9c3386906d0b4bc6fc5e0f10d6d7bd0821696100
Jan Janowicz <JayJCode@gmail.com>
2025-02-03 13:24:32 +0100

    os/sys/datetime added version without argparse

--- main.py
+++ main.py
@@ -10,15 +10,29 @@ Week 2
 6) Populate data in the text file you are working with by fetching it from an external API using requests.
 """
 import sys
+import os
+from datetime import datetime
 import argparse
 
-file_name = sys.argv[1] # 5th part. Using sys to get file_name, which we are going to open
+# 5th part. Using sys to get file_name, and os to get some information about this file.
+# Lastly using datetime for changing format to a readable for human being
+file_name = sys.argv[1]
+if not os.path.exists(file_name):
+    print("There is not a such file")
+    exit(1)
+file_size = os.stat(file_name).st_size
+file_mod_time = datetime.fromtimestamp(os.stat(file_name).st_mtime)
+print(("ABOUT CHOSEN FILE:\n" +
+       "File name: {}\n" +
+       "File size: {}\n" +
+       "Last modified: {}\n").format(file_name, file_size, file_mod_time))
 
 with open(file_name) as file:
     data = file.read() # 1st part.
     num_of_words = len(data.replace("\n", " ").strip().split(" ")) # 2nd part.
     num_of_lines = len(data.split("\n"))
     index = data.index("key") # 3rd part.
-    print(("Number of words: {}.\n" +
-           "Number of lines: {}.\n" +
-           "Index of searched word: {}.").format(num_of_words, num_of_lines, index))
+    print(("ABOUT ITS CONTENT:\n" +
+           "Number of words: {}\n" +
+           "Number of lines: {}\n" +
+           "Index of searched word: {}").format(num_of_words, num_of_lines, index))
